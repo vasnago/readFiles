617c965a5c6f801a05a2be99fe287cbf4589c79c
vasnago <vasnago_3000@yahoo.es>
2016-12-05 18:16:14 -0500

    libreries

--- ReadFiles/src/co/andrex/inc/readerAction/FrameReader.java
+++ ReadFiles/src/co/andrex/inc/readerAction/FrameReader.java
@@ -19,6 +19,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import org.apache.commons.io.FilenameUtils;
+import javax.swing.JTabbedPane;
+import javax.swing.JProgressBar;
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
 
 /**
  * This Class have the logic to the read and write the files of a path selected
@@ -33,8 +37,16 @@ public class FrameReader extends JPanel {
 	private JFrame frame;
 
 	private JLabel lblPathValue;
+	private JLabel lblPathRename;
+	private JLabel lblStatus;
 	private JFileChooser chooser;
+	private JProgressBar progressBar;
+	private JTextField textPrefix;
+	private JCheckBox chckAuto;
 	private TextArea txtAreaNameFiles;
+	private boolean flagAction = false;
+	private boolean flag;
+	private int conn = 0;
 
 	/**
 	 * Launch the application.
@@ -71,10 +83,22 @@ public class FrameReader extends JPanel {
 		String iconfilePath = this.getClass().getResource("selectFolder64.png")
 				.getFile();
 
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 415, 474);
+		frame.getContentPane().add(tabbedPane);
+
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("Search Files", null, panel_1, null);
+		panel_1.setLayout(null);
+
+		txtAreaNameFiles = new TextArea();
+		txtAreaNameFiles.setBounds(0, 97, 410, 349);
+		panel_1.add(txtAreaNameFiles);
+
 		Panel panel = new Panel();
+		panel.setBounds(0, 0, 410, 97);
+		panel_1.add(panel);
 		panel.setBackground(SystemColor.inactiveCaption);
-		panel.setBounds(0, 0, 415, 87);
-		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 
 		JButton btnSelectFolder = new JButton("");
@@ -82,6 +106,7 @@ public class FrameReader extends JPanel {
 		panel.add(btnSelectFolder);
 		btnSelectFolder.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				flagAction = false;
 				selectPath();
 			}
 		});
@@ -93,7 +118,7 @@ public class FrameReader extends JPanel {
 		JLabel lblSelectFolder = new JLabel("Select Folder");
 		lblSelectFolder.setBounds(10, 64, 89, 14);
 		panel.add(lblSelectFolder);
-		lblSelectFolder.setFont(new Font("Trebuchet MS", Font.BOLD, 13));
+		lblSelectFolder.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
 
 		JLabel lblPath = new JLabel("Path:");
 		lblPath.setBounds(99, 11, 39, 14);
@@ -101,13 +126,84 @@ public class FrameReader extends JPanel {
 		lblPath.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
 
 		lblPathValue = new JLabel("");
-		lblPathValue.setBounds(102, 31, 286, 14);
+		lblPathValue.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+		lblPathValue.setBounds(99, 39, 286, 14);
 		panel.add(lblPathValue);
 		lblPathValue.setBackground(Color.WHITE);
 
-		txtAreaNameFiles = new TextArea();
-		txtAreaNameFiles.setBounds(0, 86, 415, 378);
-		frame.getContentPane().add(txtAreaNameFiles);
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.WHITE);
+		tabbedPane.addTab("Rename Files", null, panel_2, null);
+		panel_2.setLayout(null);
+
+		Panel panel_3 = new Panel();
+		panel_3.setLayout(null);
+		panel_3.setBackground(SystemColor.inactiveCaption);
+		panel_3.setBounds(0, 0, 410, 145);
+		panel_2.add(panel_3);
+
+		JButton btnRename = new JButton("");
+		btnRename.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				flagAction = true;
+				selectPath();
+			}
+		});
+		btnRename.setIcon(new ImageIcon(iconfilePath));
+		btnRename.setFocusable(false);
+		btnRename.setContentAreaFilled(false);
+		btnRename.setBorder(BorderFactory.createEmptyBorder());
+		btnRename.setBounds(10, 11, 65, 53);
+		panel_3.add(btnRename);
+
+		JLabel label = new JLabel("Select Folder");
+		label.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
+		label.setBounds(10, 64, 83, 14);
+		panel_3.add(label);
+
+		JLabel label_1 = new JLabel("Path:");
+		label_1.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
+		label_1.setBounds(99, 11, 39, 14);
+		panel_3.add(label_1);
+
+		lblPathRename = new JLabel("");
+		lblPathRename.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+		lblPathRename.setBackground(Color.WHITE);
+		lblPathRename.setBounds(99, 39, 286, 14);
+		panel_3.add(lblPathRename);
+
+		chckAuto = new JCheckBox("Auto-Increment");
+		chckAuto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				flag = chckAuto.isSelected();
+				textPrefix.setEnabled(!flag);
+			}
+		});
+		chckAuto.setBackground(SystemColor.inactiveCaption);
+		chckAuto.setBounds(10, 85, 117, 23);
+		panel_3.add(chckAuto);
+
+		textPrefix = new JTextField();
+		textPrefix.setBounds(59, 113, 326, 20);
+		panel_3.add(textPrefix);
+		textPrefix.setColumns(10);
+
+		JLabel lblNewLabel = new JLabel("Prefix :");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel.setBounds(10, 115, 46, 14);
+		panel_3.add(lblNewLabel);
+
+		lblStatus = new JLabel("");
+		lblStatus.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		lblStatus.setBounds(115, 182, 173, 14);
+		panel_2.add(lblStatus);
+
+		progressBar = new JProgressBar();
+		progressBar.setBounds(10, 151, 390, 20);
+		progressBar.setValue(0);
+		progressBar.setMinimum(0);
+		progressBar.setStringPainted(true);
+		panel_2.add(progressBar);
 		frame.setIconImage(new ImageIcon(getClass().getResource("icon.png"))
 				.getImage());
 	}
@@ -121,7 +217,6 @@ public class FrameReader extends JPanel {
 		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		chooser.setAcceptAllFileFilterUsed(false);
 		if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-			lblPathValue.setText(chooser.getSelectedFile().getPath());
 			readFilesPath();
 		}
 	}
@@ -132,12 +227,85 @@ public class FrameReader extends JPanel {
 	private void readFilesPath() {
 		File folder = new File(chooser.getSelectedFile().getPath());
 		File[] listOfFiles = folder.listFiles();
+		String path = chooser.getSelectedFile().getPath();
+		int v = 0;
 		for (int i = 0; i < listOfFiles.length; i++) {
 			if (listOfFiles[i].isFile()) {
-				String value = changeFileName(listOfFiles[i].getName());
-				txtAreaNameFiles.append(value + "\n");
+				v++;
 			}
 		}
+		int cont = 1;
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				if (flagAction) {
+					renameFile(listOfFiles[i]);
+					int percent = (int) ((cont * 100.0f) / v);
+					progressBar.setValue(percent);
+					cont++;
+				} else {
+					lblPathValue.setText(chooser.getSelectedFile().getPath());
+					String value = changeFileName(listOfFiles[i].getName());
+					txtAreaNameFiles.append(value + "\n");
+				}
+
+			}
+		}
+		if (flagAction) {
+			lblStatus.setText("Completed Successful!");
+			lblPathRename.setText(path);
+		} else {
+			lblPathValue.setText(path);
+		}
+	}
+
+	/**
+	 * This method allows rename the file
+	 * 
+	 * @param file
+	 *            : to rename
+	 */
+	private void renameFile(File file) {
+		String path = chooser.getSelectedFile().getPath();
+		String name = changeName(file.getName());
+		File newfile = new File(path + "/" + name);
+		file.renameTo(newfile);
+	}
+
+	/**
+	 * This method allows change the name according the selected of the user
+	 * 
+	 * @param name
+	 *            : name of the file
+	 */
+	private String changeName(String name) {
+		String value = "";
+		conn++;
+		// Quitamos la extension
+		String ext = FilenameUtils.getExtension(name);
+		name = name.replaceAll("." + ext, "");
+		if (flag) {
+			value = name + "." + conn + "." + ext;
+		} else {
+			String x = name;
+			String number = "";
+			int z = x.length();
+			for (int y = z - 1; y > 0; y--) {
+				if (Character.isDigit(x.charAt(y))) {
+					char c = x.charAt(y);
+					number += c;
+				} else {
+					break;
+				}
+			}
+			String prefix = textPrefix.getText();
+			int w = number.length();
+			String number1 = "";
+			for (int y = w - 1; y >= 0; y--) {
+				number1 += number.charAt(y);
+			}
+			value = prefix + "." + number1 + "." + ext;
+		}
+		return value;
 	}
 
 	/**
@@ -152,10 +320,34 @@ public class FrameReader extends JPanel {
 		String name = FilenameUtils.getExtension(path);
 		String value = path.replaceAll("." + name, "");
 
-		// 2. Desopues remplazamos los puntos (.) por espacios en blanco
+		// 2. Despues remplazamos los puntos (.) por espacios en blanco
 		value = value.replaceAll("\\.", " ");
 		value = value.replaceAll("_", " ");
 		value = value.replaceAll("-", " ");
-		return value;
+
+		// 3. Despues se llama el metodo que detecta la mayusculas y separa el
+		// nombre.
+		String x = value;
+		String filename = "";
+		String newvalue = x;
+		int z = x.length();
+		int count = 0;
+		int sum = 0;
+		for (int y = 0; y < z; y++) {
+			int numberChart = count - 1;
+			if (Character.isUpperCase(x.charAt(y))) {
+				if (numberChart >= 0) {
+					char c = x.charAt(numberChart);
+					if (Character.isLowerCase(c)) {
+						filename = newvalue.substring(0, y + sum) + ' '
+								+ newvalue.substring(y + sum);
+						newvalue = filename;
+						sum++;
+					}
+				}
+			}
+			count++;
+		}
+		return newvalue;
 	}
 }
